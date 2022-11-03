f02cb548237e30750318af606e581a39d952b64b
olaalsh <ola_alshikh@hotmail.com>
2022-11-03 11:41:54 +0300

    first commit

--- starter/index.js
+++ starter/index.js
@@ -240,8 +240,7 @@ Hints:
 §	Value X is between 50 and 300, if it's >= 50 && <= 300 😉 
  
 GOOD LUCK 😀 
-
+*/
 const bill = 275;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20;
 console.log(`the bill was ${bill},the tip was ${tip} , and the total value ${tip+bill}`);
-*/
